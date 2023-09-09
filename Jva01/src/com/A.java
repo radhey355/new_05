07e20fcfecaf33d3866d8c3e677b59f4dc128b00
radhey35515 <saran35515@gmail.com>
2023-09-09 15:24:47 +0530

    new loine of code adde

--- Jva01/src/com/A.java
+++ Jva01/src/com/A.java
@@ -4,7 +4,8 @@ public class A {
 
 	public static void main(String[] args) {
 	
-		
+		int a=10;
+		int b=50;
 		System.out.println("absjn");
 		System.out.println("absjn");
 		System.out.println("brance");
